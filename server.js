7532121c41b6eda4f6bf2eb20ee14e6a384aff69
Steve <sbrennison@gmail.com>
2018-07-17 09:03:08 -0500

    set up server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const port = 3000;
+
+require('./db/db');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+const userController = require('./controllers/userController');
+app.use('/users', userController);
+
+const photoController = require('./controllers/photoController');
+app.use('/photos', photoController);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+});
+
+app.listen(3000, () => {
+  console.log("Express server running")
+});
